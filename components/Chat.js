27842e67c06b6fee97055de95538fceef08981ed
James Rutkowski <j.r.rutkowski52@gmail.com>
2021-08-28 22:56:05 -0400

    Initial Styling and Page added

---
+++ components/Chat.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { View, Button, Text,TextInput, StyleSheet } from "react-native";
+import { startBackground } from "../images/index"
+
+export default class Start extends Component {
+
+  componentDidMount() {
+    const name = this.props.route.params.name;
+    this.props.navigation.setOptions({ title: name });
+  }
+
+  render() {
+
+    return(
+      <View style={[styles.container,{ backgroundColor: this.props.route.params.chatColor}]}>
+        <Text style={styles.text}>Chat</Text>
+      </View>
+    );
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  text: {
+    color: "#fff"
+  }
+})
