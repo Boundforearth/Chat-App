a0f73063e37fe42e0f603d6ea48c6f450df1c7fd
James Rutkowski <j.r.rutkowski52@gmail.com>
2021-09-04 20:36:18 -0400

    removed some items from state

--- components/Chat.js
+++ components/Chat.js
@@ -13,10 +13,6 @@ export default class Chat extends Component {
     this.state = {
       messages: [],
       uid: "",
-      user: {
-        name: "",
-        _id: ""
-      }
     }
 
     //Provided firebase config data
@@ -51,10 +47,6 @@ export default class Chat extends Component {
       this.setState({
         uid: user.uid,
         messages: [],
-        user: {
-          _id: user.uid,
-          name,
-        }
       });
 
 
@@ -167,10 +159,12 @@ export default class Chat extends Component {
 
   render() {
 
+    const {chatColor, name} = this.props.route.params
+
     return(
       <View
         //sets the background color to that chosen in Start.js, also sets app to take up entire page with styles.container
-        style={[styles.container, {backgroundColor: this.props.route.params.chatColor}]}
+        style={[styles.container, {backgroundColor: chatColor}]}
       >
         <GiftedChat 
           renderSystemMessage={this.renderSystemMessage.bind(this)}
@@ -178,7 +172,10 @@ export default class Chat extends Component {
           renderBubble={this.renderBubble.bind(this)}
           messages={this.state.messages}
           onSend={(messages) => this.onSend(messages)}
-          user={this.state.user}
+          user={{
+            name,
+            _id: this.state.uid
+          }}
         />  
         {/* fixes any keyboard issues that may occur when users are on Android*/ }
         { Platform.OS === "android" ? <KeyboardAvoidingView behavior="height" /> : null }
