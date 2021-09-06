52fd3b3aa4d6990fa0f95d0dc6d26116a8465e1b
James Rutkowski <j.r.rutkowski52@gmail.com>
2021-09-06 14:57:38 -0400

    Added Image,photo, and location functionality

--- components/Chat.js
+++ components/Chat.js
@@ -4,6 +4,8 @@ import { KeyboardAvoidingView, Platform, View, StyleSheet } from "react-native";
 import { Bubble, Time, SystemMessage, GiftedChat, InputToolbar } from "react-native-gifted-chat";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import NetInfo from "@react-native-community/netinfo";
+import CustomActions from "./CustomActions"
+import MapView from 'react-native-maps';
 
 //use firebase
 const firebase = require("firebase");
@@ -16,6 +18,7 @@ export default class Chat extends Component {
       messages: [],
       uid: "",
       isConnected: false,
+      image: null,
     }
 
     //Provided firebase config data
@@ -31,8 +34,6 @@ export default class Chat extends Component {
       if (!firebase.apps.length){
         firebase.initializeApp(firebaseConfig);
         };
-
-      
       this.referenceChatMessages = firebase.firestore().collection("messages");
   }
 
@@ -125,7 +126,9 @@ export default class Chat extends Component {
         user: {
           _id: data.user._id,
           name: data.user.name,
-        }
+        },
+        image: data.image || null,
+        location: data.location || null,
       });
     });
     this.setState({
@@ -142,8 +145,10 @@ export default class Chat extends Component {
       createdAt: message.createdAt,
       user: {
         _id: this.state.uid,
-        name: message.user.name
-      }
+        name: message.user.name,
+      },
+      image: message.image || null,
+      location: message.location || null,
     })
   }
  
@@ -217,6 +222,33 @@ export default class Chat extends Component {
       )
     };
 
+    renderCustomView = props => {
+      const { currentMessage } = props;
+      if (currentMessage.location) {
+        return (
+          <MapView
+            style={{
+              width: 150,
+              height: 100,
+              borderRadius: 13,
+              margin: 3
+            }}
+            region={{
+              latitude: currentMessage.location.latitude,
+              longitude: currentMessage.location.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
+        )
+      }
+      return null;
+    }
+
+    renderCustomActions = (props) => {
+      return <CustomActions {...props} />
+    };
+
   render() {
 
     const {chatColor, name} = this.props.route.params
@@ -231,6 +263,8 @@ export default class Chat extends Component {
           renderTime={this.renderTime.bind(this)}
           renderBubble={this.renderBubble.bind(this)}
           renderInputToolbar={this.renderInputToolbar.bind(this)}
+          renderCustomView={this.renderCustomView.bind(this)}
+          renderActions={this.renderCustomActions.bind(this)}
           messages={this.state.messages}
           onSend={(messages) => this.onSend(messages)}
           user={{
---
+++ components/CustomActions.js
@@ -0,0 +1,143 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
+import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
+import * as Location from 'expo-location';
+import firebase from "firebase";
+import { firestore } from "firebase";
+
+export default class CustomActions extends Component {
+
+  pickImage = async () => {
+    const {status} = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    if(status === "granted") {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: "Images",
+      }).catch(error => console.log(error));
+      if(!result.cancelled) {
+        const imageUrl = await this.uploadImageFetch(result.uri);
+        this.props.onSend({image: imageUrl});
+      }
+    }
+  }
+
+  takePhoto = async() => {
+    const {status} = await Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.CAMERA);
+    if(status === "granted") {
+      let result = await ImagePicker.launchCameraAsync().catch(error => console.log(error));
+
+      if(!result.cancelled) {
+        const imageUrl = await this.uploadImageFetch(result.uri);
+        this.props.onSend({image: imageUrl});
+      }
+    }
+  }
+
+  getLocation = async() => {
+    const {status} = await Permissions.askAsync(Permissions.LOCATION);
+    if(status === "granted") {
+      let result = await Location.getCurrentPositionAsync({}).catch(error => console.log(error));
+      if (result) {
+        this.props.onSend({
+          location: {
+            longitude: result.coords.longitude,
+            latitude: result.coords.latitude,
+          },
+        });
+      }
+    }
+  }
+
+  onActionPress = () => {
+    const options = ["Choose From Library", "Take Picture", "Send Location", "Cancel"];
+    const cancelButtonIndex = options.length - 1;
+    this.context.actionSheet().showActionSheetWithOptions(
+      {
+        options,
+        cancelButtonIndex,
+      },
+      async (buttonIndex) => {
+        switch (buttonIndex) {
+          case 0:
+            this.pickImage();
+            return;
+          case 1:
+            this.takePhoto();
+            return;
+          case 2:
+            this.getLocation();
+          default:
+        }
+      },
+    );
+  };
+
+  uploadImageFetch = async (uri) => {
+    const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function() {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function (e) {
+        console.log(e);
+        reject(new TypeError("Network request failed"));
+      };
+      xhr.responseType = "blob";
+      xhr.open("GET", uri, true);
+      xhr.send(null);
+    });
+    const imageNameBefore = uri.split("/");
+    const imageName = imageNameBefore[imageNameBefore.length - 1];
+
+    const ref = firebase.storage().ref().child(`images/${imageName}`);
+
+    const snapshot = await ref.put(blob);
+
+    blob.close();
+
+    return await snapshot.ref.getDownloadURL();
+  }
+
+  render() {
+    return(
+      <TouchableOpacity
+        style={[styles.container]}
+        onPress={this.onActionPress}
+        accessible={true}
+        accessibilityLabel="Choose image, camera, location, or cancel"
+        accessibilityHint="Upload an image to the chat, take a picture to send, or send your location"
+      >
+        <View style={[styles.wrapper, this.props.wrapperStyle]}>
+          <Text style={[styles.iconText, this.props.iconTextStyle]}>+</Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 26,
+    height: 26,
+    marginLeft: 10,
+    marginBottom: 10,
+  },
+  wrapper: {
+    borderRadius: 13,
+    borderColor: "#b2b2b2",
+    borderWidth: 2,
+    flex: 1,
+  },
+  iconText: {
+    color: "#b2b2b2",
+    fontWeight: "bold",
+    fontSize: 16,
+    backgroundColor: "transparent",
+    textAlign: "center",
+  },
+});
+
+CustomActions.contextTypes = {
+  actionSheet: PropTypes.func,
+}
